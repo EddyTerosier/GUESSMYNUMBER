abd6821f3faa9fba6ad07b3fd9aa01f0a83811c7
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-02 12:09:37 +0100

    Part 1 Handling Click Events

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 55;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function(){
+    const guess= Number(document.querySelector(".guess").value);
+    console.log(guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent = '🦍 No number!';
+    }
+}); 
